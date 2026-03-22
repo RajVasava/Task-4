40faa0faddeeb3816cb98ab5e8640629e8d1f593
Raj Vasava <rajbca65a@gmail.com>
2026-03-22 14:32:33 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Analytics from "./Analytics";
+
+function App() {
+  return (
+    <div>
+      <h2>Weekly Productivity Report</h2>
+      <Analytics />
+    </div>
+  );
+}
+export default App;
