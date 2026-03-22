0e549b0f19b3ab92e297574248131f33fb7d4c4b
Raj Vasava <rajbca65a@gmail.com>
2026-03-22 14:14:23 +0530

    Add files via upload

---
+++ Frontend/Analytics.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react"; 
+import axios from "axios"; 
+import { Pie } from "react-chartjs-2"; 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"; 
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function Analytics() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/analytics").then(res => setData(res.data));
+  }, []);
+
+  const productiveSites = ["github.com", "stackoverflow.com", "leetcode.com"];
+  const unproductiveSites = ["facebook.com", "instagram.com", "youtube.com"];
+
+  let productive = 0, unproductive = 0;
+  data.forEach(log => {
+    if (productiveSites.some(s => log.url.includes(s))) productive += log.duration;
+    else if (unproductiveSites.some(s => log.url.includes(s))) unproductive += log.duration;
+  });
+
+  const chartData = {
+    labels: ["Productive", "Unproductive"],
+    datasets: [{ data: [productive, unproductive], backgroundColor: ["green", "red"] }]
+  };
+
+  return <Pie data={chartData} />;
+  
+}
+
